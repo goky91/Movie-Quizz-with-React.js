5900f5cdaa059ecc248110b059d194142dd3b563
Goran <goky91@gmail.com>
2020-05-11 17:47:57 +0200

    Additional files cleanup.

--- src/Services/GenerateMovieCard.js
+++
@@ -1,9 +0,0 @@
-const API = `54e4b3f735dc7e796075865340b11815`;
-
-const req = `https://api.themoviedb.org/3/movie/550?api_key=` + API;
-
-axios.get(req)
-      .then(res => {
-        const movieInfo = res.data;
-        this.setState({ persons });
-      })
